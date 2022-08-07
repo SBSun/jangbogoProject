63b293638bfb9fc8afe4e28a43bee035a9f2fffe
SBSun <46879319+SBSun@users.noreply.github.com>
2022-08-07 21:20:31 +0900

    Initial commit

---
+++ src/main/java/backend/jangbogoProject/JangbogoProjectApplication.java
@@ -0,0 +1,13 @@
+package backend.jangbogoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangbogoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JangbogoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/jangbogoProject/JangbogoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.jangbogoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JangbogoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
