8874d1d1eea5d7cddeb74b72723dbcdb87a43bf8
SBSun <46879319+SBSun@users.noreply.github.com>
2022-08-07 21:59:32 +0900

    Add HomeController

---
+++ src/main/java/backend/jangbogoProject/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package backend.jangbogoProject.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HomeController {
+    // http://localhost:8080
+    @GetMapping("/")
+    public ModelAndView home()
+    {
+        ModelAndView mav = new ModelAndView("home/home");
+        return mav;
+    }
+}
