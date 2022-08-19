fdd9f6e6dab91374f0869d06b01704b2659d2049
SBSun <46879319+SBSun@users.noreply.github.com>
2022-08-20 00:04:26 +0900

    ResponseBody 테스트

--- src/main/java/backend/jangbogoProject/Controller/HomeController.java
+++ src/main/java/backend/jangbogoProject/Controller/HomeController.java
@@ -2,6 +2,7 @@ package backend.jangbogoProject.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,6 +11,7 @@ public class HomeController {
 
     // http://localhost:8080
     @GetMapping("/")
+    @ResponseBody
     public ModelAndView home()
     {
         ModelAndView mav = new ModelAndView("index");
