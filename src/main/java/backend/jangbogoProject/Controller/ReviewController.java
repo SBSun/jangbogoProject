b7e6203d87e7750c48eb6008674058871cb6ab70
SBSun <46879319+SBSun@users.noreply.github.com>
2022-08-20 23:06:02 +0900

    1. ReviewController 구현 - 마켓, 사용자 리뷰, 리뷰 작성 이동 가능

---
+++ src/main/java/backend/jangbogoProject/Controller/ReviewController.java
@@ -0,0 +1,30 @@
+package backend.jangbogoProject.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class ReviewController {
+
+    // http://localhost:8080/review/market
+    @GetMapping("/review/market")
+    public ModelAndView market(){
+        ModelAndView mav = new ModelAndView("review/market");
+        return mav;
+    }
+
+    // http://localhost:8080/review/user
+    @GetMapping("/review/user")
+    public ModelAndView userReview(){
+        ModelAndView mav = new ModelAndView("review/user");
+        return mav;
+    }
+
+    // http://localhost:8080/review/write
+    @GetMapping("/review/write")
+    public ModelAndView write(){
+        ModelAndView mav = new ModelAndView("review/write");
+        return mav;
+    }
+}
