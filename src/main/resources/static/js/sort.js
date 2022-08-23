048efdcac9644f4cc673e8d7450b89f3b44e5f9c
hyeonpearl <hc9064@gmail.com>
2022-08-23 18:13:26 +0900

    Fix: 정렬 기능 코드 수정

--- src/main/resources/static/js/sort.js
+++ src/main/resources/static/js/sort.js
@@ -1,38 +1,26 @@
-'use strict';
+let subMenu = document.querySelector('.dropdown_subMenu');
+let sortBtn = document.getElementById('sortBtn');
 
-// 정렬 기준
-{
-  let subMenu = document.getElementsByClassName('dropdown_subMenu')[0];
-  let sortBtn = document.getElementById('sortBtn');
+let favorit = document.getElementById('sort_favorit');
+let low = document.getElementById('sort_low');
+let high = document.getElementById('sort_high');
 
-  sortBtn.addEventListener('click', clickSort);
-
-  function clickSort() {
-    if (subMenu.style.display == 'block') {
-      subMenu.style.display = 'none';
-    } else {
-      subMenu.style.display = 'block';
-    }
-  }
-
-  let favorit = document.getElementById('sort_favorit');
-  let low = document.getElementById('sort_low');
-  let high = document.getElementById('sort_high');
-
-  favorit.addEventListener('click', setFavor);
-  low.addEventListener('click', setLow);
-  high.addEventListener('click', setHigh);
-
-  function setFavor() {
-    sortBtn.innerText = '찜 많은 순';
+function clickSort() {
+  if (subMenu.style.display == 'block') {
     subMenu.style.display = 'none';
-  }
-  function setLow() {
-    sortBtn.innerText = '낮은 가격순';
-    subMenu.style.display = 'none';
-  }
-  function setHigh() {
-    sortBtn.innerText = '높은 가격순';
-    subMenu.style.display = 'none';
+  } else {
+    subMenu.style.display = 'block';
   }
 }
+function setLow() {
+  sortBtn.innerText = '낮은 가격순';
+  subMenu.style.display = 'none';
+}
+function setHigh() {
+  sortBtn.innerText = '높은 가격순';
+  subMenu.style.display = 'none';
+}
+
+sortBtn.addEventListener('click', clickSort);
+low.addEventListener('click', setLow);
+high.addEventListener('click', setHigh);
