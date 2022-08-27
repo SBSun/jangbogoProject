96c154d4540f7be3651645c1eb667eb616083267
SBSun <46879319+SBSun@users.noreply.github.com>
2022-08-27 22:40:32 +0900

    Category Entity 구현

---
+++ src/main/java/backend/jangbogoProject/category/Category.java
@@ -0,0 +1,35 @@
+package backend.jangbogoProject.category;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category {
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn (name ="parent_id")
+    private Category parentCategory;
+
+    @OneToMany (mappedBy = "parentCategory", cascade = CascadeType.ALL)
+    private List<Category> subCategory = new ArrayList<>();
+
+    private Integer level;
+
+    public Category(String name, Category parentCategory, Integer level) {
+        this.name = name;
+        this.parentCategory = parentCategory;
+        this.level = level;
+    }
+}
