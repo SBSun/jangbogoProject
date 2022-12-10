181bf4b9a73365ea8845fe284b6f2765d46d5e89
SBSun <46879319+SBSun@users.noreply.github.com>
2022-12-10 11:47:33 +0900

    User - UserDeatils Interface 적용

--- src/main/java/backend/jangbogoProject/security/CustomUserDetailsService.java
+++ src/main/java/backend/jangbogoProject/security/CustomUserDetailsService.java
@@ -11,9 +11,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @Service
 @AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
@@ -22,14 +19,18 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String id) {
-
-        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-
         User user = userRepository.findOneById(id);
 
         if (user != null) {
-            grantedAuthorities.add(new SimpleGrantedAuthority("USER")); // USER 라는 역할을 넣어준다.
-            return new User(user.getId(), user.getPassword(), grantedAuthorities);
+            // USER 라는 역할을 넣어준다.
+
+            User authUser = User.builder()
+                    .id(user.getId())
+                    .password(user.getPassword())
+                    .name(user.getName())
+                    .address(user.getAddress())
+                    .role("User").build();
+            return authUser;
         } else {
             throw new UsernameNotFoundException("can not find User : " + id);
         }
--- src/main/java/backend/jangbogoProject/user/User.java
+++ src/main/java/backend/jangbogoProject/user/User.java
@@ -3,13 +3,18 @@ package backend.jangbogoProject.user;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Entity
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -20,13 +25,50 @@ public class User {
     private String name;
     @Column(nullable = false)
     private String address;
-    
+    @Column(nullable = false)
+    private String role;
 
     @Builder
-    public User(String id, String password, String name, String address) {
+    public User(String id, String password, String name, String address, String role) {
         this.id = id;
         this.password = password;
         this.name = name;
         this.address = address;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+        for(String role : role.split(",")){
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return id;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
