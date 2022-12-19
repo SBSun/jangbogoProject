012c4959ba22efd0e17a26a14e8e34a292547288
hyeonpearl <hc9064@gmail.com>
2022-12-19 22:12:32 +0900

    test: React, Spring 연동 테스트
    Spring 빌드 후 frontend 폴더 콘솔에서 npm start로 React 빌드

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,18 @@
+import React, { useEffect, useState } from 'react';
+
 const App = () => {
+  const [hello, setHello] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(response => setHello(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
-    <div className='App'>
-      <h1>Hello from Frontend!</h1>
+    <div>
+      <h2>Hello from Frontend!</h2>
+      <h2>{hello}</h2>
     </div>
   );
 };
