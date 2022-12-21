595adde31af69d1dfcf029d519fd6b5ee5a38b9d
hyeonpearl <hc9064@gmail.com>
2022-12-21 13:49:23 +0900

    init: React, Spring 연동
    서버 연동 완료하여 테스트에 사용한
    파일들 전부 초기화

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,18 +1,9 @@
 import React, { useEffect, useState } from 'react';
 
 const App = () => {
-  const [hello, setHello] = useState('');
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(response => setHello(response.data))
-      .catch(error => console.log(error));
-  }, []);
-
   return (
     <div>
-      <h2>Hello from Frontend!</h2>
-      <h2>{hello}</h2>
+      <h2>Hello</h2>
     </div>
   );
 };
