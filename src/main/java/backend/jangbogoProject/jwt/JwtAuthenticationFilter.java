eeadfdb9b22396a1201c78a26410cc6011355224
SBSun <46879319+SBSun@users.noreply.github.com>
2022-12-23 12:52:42 +0900

    Add JwtAuthenticationFilter Class

---
+++ src/main/java/backend/jangbogoProject/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,51 @@
+package backend.jangbogoProject.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 1. Request Header 에서 JWT 토큰 추출
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String token = resolveToken(httpServletRequest);
+        String requestURI = httpServletRequest.getRequestURI();
+
+        // 2. validateToken 으로 토큰 유효성 검사
+        if(StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)){
+            // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext 에 저장
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            LOGGER.info("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+        }else{
+            LOGGER.info("유효한 JWT 토큰이 없습니다., uri: {}", requestURI);
+        }
+        chain.doFilter(httpServletRequest, response);
+    }
+
+    // Request Header 에서 토큰 정보 추출
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7); // "Bearer " 이후의 ACCESS_TOKEN 문자열 반환
+        }
+        return null;
+    }
+}
