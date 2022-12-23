6bc00f2740d6a86483f804ebc753a0e91dc4831e
SBSun <46879319+SBSun@users.noreply.github.com>
2022-12-23 13:35:39 +0900

    Update SecurityConfiguration Class - JWT 설정 추가

--- src/main/java/backend/jangbogoProject/security/SecurityConfiguration.java
+++ src/main/java/backend/jangbogoProject/security/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package backend.jangbogoProject.security;
 
+import backend.jangbogoProject.jwt.JwtAuthenticationFilter;
+import backend.jangbogoProject.jwt.JwtTokenProvider;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -7,11 +9,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
@@ -24,6 +28,8 @@ public class SecurityConfiguration {
     // 로그인 실패 핸들러 의존성 주입
     private final CustomAuthFailureHandler customFailureHandler;
 
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Bean
     public static BCryptPasswordEncoder bCryptPasswordEncoder() {
         //  AuthenticationManagerBuilder에  패스워드 암호화를 위해 Spring Security에서 제공하는 BCryptPasswordEncoder를 추가
@@ -33,12 +39,20 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         /* @formatter:off */
-        http.csrf().disable()
+        http
+                // rest api이므로 basic auth 및 csrf 보안을 사용하지 않는다는 설정이다.
+                .httpBasic().disable()
+                .csrf().disable()
+                // JWT를 사용하기 때문에 세션을 사용하지 않는다는 설정
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
                 .antMatchers("/", "/signUp").permitAll() // 설정한 리소스의 접근을 인증절차 없이 허용
                 .antMatchers("/admin", "/signUpAdmin", "/data_load_save").hasRole("ADMIN")
                 .antMatchers("/signUpAdmin").hasRole("USER")
                 .and()
+                //  JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행하겠다는 설정이다.
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
             .formLogin()
                 .loginPage("/") // 기본 로그인 페이지
                 .defaultSuccessUrl("/")
