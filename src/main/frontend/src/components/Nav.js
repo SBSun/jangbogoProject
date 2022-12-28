7a6507d382bd90c63ff85b54139881bcc1d4882e
hyeonpearl <hc9064@gmail.com>
2022-12-28 13:56:56 +0900

    Add: Logo.svg 추가

--- src/main/frontend/src/components/Nav.js
+++ src/main/frontend/src/components/Nav.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './Nav.module.css';
+import logo from '../assets/Logo.svg';
 
 const Nav = () => {
   return (
     <nav>
       <ul>
-        <li>Icon & Logo</li>
         <li>
           <a href='#' id='loginBtn'>
             Log in
@@ -14,6 +14,11 @@ const Nav = () => {
         <li>
           <a href='#'>
             <i className='fa-solid fa-magnifying-glass'></i>
+          </a>
+        </li>
+        <li>
+          <a href='#' className='nav_loginBtn'>
+            Log in
           </a>
         </li>
       </ul>
