b5170a689bddcd1797ab78a731b4a689d0e053df
hyeonpearl <hc9064@gmail.com>
2023-01-06 21:52:53 +0900

    Style: 디자인 작업
    모바일 해상도가 아닐 경우, 경고 문구 출력

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
+import styled from 'styled-components';
 import GlobalStyle from './GlobalStyle';
 import Loading from './routes/Loading';
 import Home from './routes/Home';
@@ -14,10 +15,23 @@ import Market from './routes/Market';
 import Review from './routes/Review';
 
 const App = () => {
+  const ScreenSize = styled.div`
+    display: none;
+    width: 100vw;
+    font-size: 40px;
+    vertical-align: middle;
+    text-align: center;
+    background-color: var(--black);
+    color: white;
+    @media screen and (min-width: 600px) {
+      display: block;
+    }
+  `;
   const [isLoading, setIsLoading] = useState(true);
   return (
     <BrowserRouter>
       <GlobalStyle />
+      <ScreenSize>모바일 해상도만 지원 중입니다.</ScreenSize>
       {isLoading ? (
         <Loading />
       ) : (
