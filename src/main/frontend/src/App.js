372989f6b59c09e98d8f7567e7a9c9b080a943ae
hyeonpearl <hc9064@gmail.com>
2023-01-10 17:58:33 +0900

    Rename: 라우터 이름, 경로 수정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,37 +1,31 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import styled from 'styled-components';
 import GlobalStyle from './GlobalStyle';
-import Loading from './routes/Loading';
 import Home from './routes/Home';
 import Category from './routes/Category';
 import Search from './routes/Search';
 import Mypage from './routes/MyPage';
-import SignIn from './routes/SignIn';
+import LogIn from './routes/LogIn';
 import SignUp from './routes/SignUp';
 import Market from './routes/Market';
 import Review from './routes/Review';
+import Info from './routes/Info';
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(false);
   return (
     <BrowserRouter>
       <GlobalStyle />
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/category' element={<Category />} />
-          <Route path='/search' element={<Search />} />
-          <Route path='/mypage' element={<Mypage />} />
-          <Route path='/signin' element={<SignIn />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/market' element={<Market />} />
-          <Route path='/review' element={<Review />} />
-        </Routes>
-      )}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/category' element={<Category />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/mypage' element={<Mypage />} />
+        <Route path='/mypage/info' element={<Info />} />
+        <Route path='/member/login' element={<LogIn />} />
+        <Route path='/member/signup' element={<SignUp />} />
+        <Route path='/market' element={<Market />} />
+        <Route path='/market/review' element={<Review />} />
+      </Routes>
     </BrowserRouter>
   );
 };
