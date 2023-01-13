73d5c341df222d88b821ef5bfa89d7bb215642a2
hyeonpearl <hc9064@gmail.com>
2023-01-13 17:24:20 +0900

    Style: Category CSS 작업

--- src/main/frontend/src/routes/Category.js
+++ src/main/frontend/src/routes/Category.js
@@ -1,33 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Header from '../components/Header';
 import Navigation from '../components/Navigation';
 
 const Container = styled.main`
   margin: 56px 0 0 0;
-
-  > ul > .category_menu {
+`;
+const MenuList = styled.ul`
+  > .category_menu {
     display: flex;
     justify-content: space-between;
     font-size: 1.25rem;
     padding: 1.25rem;
     border-bottom: 1px solid var(--gray);
-  }
-  > ul > .category_menu > i:nth-child(2) {
-    display: none;
-  }
-`;
-const Detail = styled.section`
-  background-color: var(--light-gray);
-  color: var(--black);
-  padding: 1rem 1rem 1rem 4rem;
-  border-bottom: 1px solid var(--gray);
-
-  > ul > li {
-    margin: 0 0 1rem 0;
-  }
-  > ul > li:last-child {
-    margin: 0;
   }
 `;
 
@@ -39,60 +24,24 @@ const Category = () => {
     <>
       <Header />
       <Container>
-        <ul>
+        <MenuList>
           <li className='category_menu' onClick={handleClick}>
             <span>정육 ・ 계란</span>
-            <i className='fa-solid fa-angle-down'></i>
-            <i className='fa-solid fa-angle-up'></i>
+            <i className='fa-solid fa-angle-right'></i>
           </li>
-          <Detail>
-            <ul>
-              <li>전체보기</li>
-              <li>전체보기</li>
-              <li>전체보기</li>
-              <li>전체보기</li>
-            </ul>
-          </Detail>
           <li className='category_menu' onClick={handleClick}>
             <span>수산 ・ 해산물</span>
-            <i className='fa-solid fa-angle-down'></i>
-            <i className='fa-solid fa-angle-up'></i>
+            <i className='fa-solid fa-angle-right'></i>
           </li>
-          <Detail>
-            <ul>
-              <li>전체보기</li>
-              <li>전체보기</li>
-              <li>전체보기</li>
-              <li>전체보기</li>
-            </ul>
-          </Detail>
-          <li className='category_menu' onClick={handleClick}>
-            <span>과일</span>
-            <i className='fa-solid fa-angle-down'></i>
-            <i className='fa-solid fa-angle-up'></i>
-          </li>
-          <Detail>
-            <ul>
-              <li>전체보기</li>
-              <li>전체보기</li>
-              <li>전체보기</li>
-              <li>전체보기</li>
-            </ul>
-          </Detail>
           <li className='category_menu' onClick={handleClick}>
             <span>채소</span>
-            <i className='fa-solid fa-angle-down'></i>
-            <i className='fa-solid fa-angle-up'></i>
+            <i className='fa-solid fa-angle-right'></i>
           </li>
-          <Detail>
-            <ul>
-              <li>전체보기</li>
-              <li>전체보기</li>
-              <li>전체보기</li>
-              <li>전체보기</li>
-            </ul>
-          </Detail>
-        </ul>
+          <li className='category_menu' onClick={handleClick}>
+            <span>과일</span>
+            <i className='fa-solid fa-angle-right'></i>
+          </li>
+        </MenuList>
       </Container>
       <Navigation />
     </>
