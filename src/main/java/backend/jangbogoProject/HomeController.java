b2cebbba4514b1d499853b162fa1e44ad9dca35d
SBSun <46879319+SBSun@users.noreply.github.com>
2023-01-15 23:23:11 +0900

    Mypage.js -> MyPage.js

--- src/main/java/backend/jangbogoProject/HomeController.java
+++ src/main/java/backend/jangbogoProject/HomeController.java
@@ -4,6 +4,7 @@ import backend.jangbogoProject.commodity.CommodityService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,14 +13,6 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController {
 
     private final CommodityService commodityService;
-
-    @GetMapping("/")
-    public ModelAndView homeForm()
-    {
-        ModelAndView mav = new ModelAndView("index");
-
-        return mav;
-    }
 
     @PostMapping("/data_load_save")
     public ModelAndView data_load_save(){
