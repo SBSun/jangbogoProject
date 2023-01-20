586daa142da3961b8d91686e1dbcfc113d89bc50
hyeonpearl <hc9064@gmail.com>
2023-01-20 18:19:15 +0900

    Refactor: 코드 리팩토링

--- src/main/frontend/src/routes/Category.js
+++ src/main/frontend/src/routes/Category.js
@@ -1,47 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
+import { MdOutlineNavigateNext } from 'react-icons/md';
 import Header from '../components/Header';
 import Navigation from '../components/Navigation';
 
 const Container = styled.main`
   margin: 56px 0 0 0;
 `;
-const MenuList = styled.ul`
-  > .category_menu {
-    display: flex;
-    justify-content: space-between;
-    font-size: 1.25rem;
-    padding: 1.25rem;
-    border-bottom: 1px solid var(--gray);
-  }
+const CategoryItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  font-size: 1.25rem;
+  padding: 1.25rem;
+  border-bottom: 1px solid var(--gray);
 `;
 
 const Category = () => {
   const handleClick = e => {
-    console.log(e.target);
+    console.log(e);
   };
   return (
     <>
       <Header modify={1} title={'카테고리'} />
       <Container>
-        <MenuList>
-          <li className='category_menu' onClick={handleClick}>
+        <ul>
+          <CategoryItem onClick={handleClick}>
             <span>정육 ・ 계란</span>
-            <i className='fa-solid fa-angle-right'></i>
-          </li>
-          <li className='category_menu' onClick={handleClick}>
+            <MdOutlineNavigateNext />
+          </CategoryItem>
+          <CategoryItem onClick={handleClick}>
             <span>수산 ・ 해산물</span>
-            <i className='fa-solid fa-angle-right'></i>
-          </li>
-          <li className='category_menu' onClick={handleClick}>
+            <MdOutlineNavigateNext />
+          </CategoryItem>
+          <CategoryItem onClick={handleClick}>
             <span>채소</span>
-            <i className='fa-solid fa-angle-right'></i>
-          </li>
-          <li className='category_menu' onClick={handleClick}>
+            <MdOutlineNavigateNext />
+          </CategoryItem>
+          <CategoryItem onClick={handleClick}>
             <span>과일</span>
-            <i className='fa-solid fa-angle-right'></i>
-          </li>
-        </MenuList>
+            <MdOutlineNavigateNext />
+          </CategoryItem>
+        </ul>
       </Container>
       <Navigation />
     </>
