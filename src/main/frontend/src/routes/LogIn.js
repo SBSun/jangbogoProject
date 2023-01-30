f2ca3d8ebec4819ec9997d346afda31f58b42f87
hyeonpearl <hc9064@gmail.com>
2023-01-30 17:55:48 +0900

    Feat: 로그인 기능 구현 (미완성)
    새로고침 시 초기화됨, JWT 적용 X

--- src/main/frontend/src/routes/LogIn.js
+++ src/main/frontend/src/routes/LogIn.js
@@ -1,10 +1,11 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from '../components/Header';
 import Button from '../components/Button';
+import UserContext from '../contexts/user';
 
-const LoginContainer = styled.main`
+const LoginBlock = styled.main`
   margin: 56px 0 0 0;
 `;
 const LoginForm = styled.form`
@@ -32,6 +33,7 @@ const LoginForm = styled.form`
 
 const Login = () => {
   const navigate = useNavigate();
+  const { actions } = useContext(UserContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -44,30 +46,46 @@ const Login = () => {
   const onSubmit = useCallback(
     e => {
       e.preventDefault();
+
       const getLogin = async () => {
-        const post = await (
-          await fetch(`/user/login`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              id: email,
-              password: password,
-            }),
-          })
-        ).json();
+        const response = await fetch(`/user/login`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            id: email,
+            password: password,
+          }),
+        });
+
+        if (!response.ok) {
+          const message = `다시 로그인해주세요.`;
+          alert(message);
+          throw new Error(message);
+        }
+
+        const post = await response.json();
         console.log(post);
+        actions.setIsLogin(true);
+        actions.setToken({
+          accessToken: post.accessToken,
+          refreshToken: post.refreshToken,
+        });
+        alert('로그인되었습니다.');
+        navigate('/', true);
       };
+
       getLogin();
       setEmail('');
       setPassword('');
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [email, password]
   );
 
   return (
     <>
       <Header modify={'WHITE_BLOCK'} title={'로그인'} />
-      <LoginContainer>
+      <LoginBlock>
         <LoginForm onSubmit={onSubmit}>
           <input
             type={'text'}
@@ -90,7 +108,7 @@ const Login = () => {
             회원가입
           </Button>
         </LoginForm>
-      </LoginContainer>
+      </LoginBlock>
     </>
   );
 };
