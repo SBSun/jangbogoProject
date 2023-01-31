f41a6c391e739fedabb3690914e06d00dfbe758b
hyeonpearl <hc9064@gmail.com>
2023-01-31 21:54:49 +0900

    Feat: 지역 설정 기능 추가 (미완성)

--- src/main/frontend/src/components/SelectLocation.js
+++ src/main/frontend/src/components/SelectLocation.js
@@ -10,20 +10,49 @@ const LocationBlock = styled.div`
   right: 0;
   height: 60vh;
   background: white;
-  border-top: 1px solid var(--light-gray);
+  border-top: 1px solid var(--gray);
   text-align: center;
+  overflow-y: scroll;
   z-index: 3;
+
+  > .location-title {
+    position: fixed;
+    left: 0;
+    right: 0;
+    text-align: center;
+    font-size: 1.25rem;
+    font-weight: 800;
+    padding: 1rem;
+    border-bottom: 1px solid var(--gray);
+    color: white;
+    background: var(--green);
+  }
 
   ${props =>
     props.isVisible &&
     css`
       display: block;
+
+      body {
+        filter: blur(15px);
+      }
     `}
 `;
-const LocationList = styled.ul``;
+const LocationList = styled.ul`
+  margin: 53px 0 0 0;
+  padding: 1rem;
+
+  > li {
+    padding: 1rem;
+    width: 30%;
+    display: inline-block;
+    text-align: center;
+    cursor: pointer;
+  }
+`;
 
 const Location = () => {
-  const { state } = useContext(LocationContext);
+  const { state, actions } = useContext(LocationContext);
   const [locationList, setLocationList] = useState([]);
 
   useEffect(() => {
@@ -37,14 +66,29 @@ const Location = () => {
     console.log(response.guInfoList);
   };
   const locationListItem = locationList.map(item => (
-    <li key={item.gu_Id}>{item.name}</li>
+    <li
+      key={item.gu_Id}
+      onClick={() => {
+        actions.setLocation({
+          id: item.gu_Id,
+          name: item.name,
+        });
+        console.log(
+          `Set Location : [id : ${state.location.id}] [name : ${state.location.name}]`
+        );
+        actions.setIsVisible(false);
+      }}
+    >
+      {item.name}
+    </li>
   ));
 
-  // if (state.location.id === null) {
-  //   actions.setIsVisible(true);
-  // }
+  if (state.location.id === null) {
+    actions.setIsVisible(true);
+  }
   return (
     <LocationBlock isVisible={state.isVisible}>
+      <h2 className='location-title'>지역을 선택해주세요.</h2>
       <LocationList>{locationListItem}</LocationList>
     </LocationBlock>
   );
