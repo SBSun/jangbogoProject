11739c4e38a696576f149613e5ddee0fdd73cb9a
hyeonpearl <hc9064@gmail.com>
2023-02-04 16:16:51 +0900

    Add: auth 모듈 작성

--- src/main/frontend/src/modules/auth.js
+++ src/main/frontend/src/modules/auth.js
@@ -1,14 +1,48 @@
 import { createAction, handleActions } from 'redux-actions';
 
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+const POST_REGISTER = 'auth/POST_REGISTER';
+const POST_LOGIN = 'auth/POST_LOGIN';
 
-export const sampleAction = createAction(SAMPLE_ACTION);
+export const postRegister = createAction(POST_REGISTER, input => ({
+  register: {
+    email: input.email,
+    password: input.password,
+    paswordConfirm: input.paswordConfirm,
+    name: input.name,
+    address: input.address,
+  },
+}));
+export const postLogin = createAction(POST_LOGIN, input => ({
+  login: { email: input.email, password: input.password },
+}));
 
-const initialState = {};
+const initialState = {
+  register: {
+    email: '',
+    password: '',
+    paswordConfirm: '',
+    name: '',
+    address: '',
+  },
+  login: {
+    email: '',
+    password: '',
+  },
+};
 
 const auth = handleActions(
   {
-    [SAMPLE_ACTION]: (state, action) => state,
+    [POST_REGISTER]: (state, action) => ({
+      email: state.email,
+      password: state.password,
+      paswordConfirm: state.passwordConfirm,
+      name: state.name,
+      address: state.address,
+    }),
+    [POST_LOGIN]: (state, action) => ({
+      email: state.email,
+      password: state.password,
+    }),
   },
   initialState
 );
