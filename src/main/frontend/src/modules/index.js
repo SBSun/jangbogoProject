68890cb645bfab79b8be79006527e27f9e450b24
hyeonpearl <hc9064@gmail.com>
2023-02-09 16:43:50 +0900

    Fix: 안쓰는 코드 수정

--- src/main/frontend/src/modules/index.js
+++ src/main/frontend/src/modules/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
-import loading from './loading';
 
 const rootReducer = combineReducers({
   auth,
-  loading,
 });
 
 export default rootReducer;
