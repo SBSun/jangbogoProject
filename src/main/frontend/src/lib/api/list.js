c76013d1a5c3c02bea47b04f679d2c6b8389698c
hyeonpearl <hc9064@gmail.com>
2023-02-14 16:49:49 +0900

    Fix: 불필요한 코드 수정

--- src/main/frontend/src/lib/api/list.js
+++ src/main/frontend/src/lib/api/list.js
@@ -1,5 +1,6 @@
 import client from './client';
 
+// 품목 리스트 API
 export const getCommodityList = async (
   id,
   curPage,
@@ -10,15 +11,16 @@ export const getCommodityList = async (
   const res = await client.get(
     `/commodity/getCommodities?gu_id=${id}&curPage=${curPage}&recordSize=${recordSize}&keyword=${keyword}&searchType=${searchType}`
   );
-  console.log(res.data);
   return res.data.infoList;
 };
 
+// 매장 리스트 API
 export const getMarketList = async id => {
   const res = await client.get(`/market/findMarketsInGu?gu_id=${id}`);
   return res.data;
 };
 
+// 지역 리스트 API
 export const getLocationList = async () => {
   const res = await client.get(`/gu/findAllGuInfo`);
   return res.data.guInfoList;
