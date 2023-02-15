7d8ec933ea0f32eb8e2f930d15557e86f520854d
hyeonpearl <hc9064@gmail.com>
2023-02-15 17:29:43 +0900

    Add: 품목 관련 API 추가

--- src/main/frontend/src/lib/api/list.js
+++ src/main/frontend/src/lib/api/list.js
@@ -1,17 +1,27 @@
 import client from './client';
 
 // 품목 리스트 API
-export const getCommodityList = async (
-  id,
-  curPage,
-  recordSize,
-  keyword,
-  searchType
-) => {
+export const getCommodityList = async (id, curPage, recordSize) => {
   const res = await client.get(
-    `/commodity/getCommodities?gu_id=${id}&curPage=${curPage}&recordSize=${recordSize}&keyword=${keyword}&searchType=${searchType}`
+    `/commodity/getCommodities?gu_id=${id}&curPage=${curPage}&recordSize=${recordSize}`
   );
-  return res.data.infoList;
+  return res.data;
+};
+
+// 카테고리 별 품목 리스트 API
+export const getCatagoryList = async (id, curPage, recordSize, keyword) => {
+  const res = await client.get(
+    `/commodity/findByCategory?gu_id=${id}&curPage=${curPage}&recordSize=${recordSize}&keyword=${keyword}`
+  );
+  return res.data;
+};
+
+// 검색 품목 리스트 API
+export const getSearchList = async (id, curPage, recordSize, keyword) => {
+  const res = await client.get(
+    `/commodity/findByKeyword?gu_id=${id}&curPage=${curPage}&recordSize=${recordSize}&keyword=${keyword}`
+  );
+  return res.data;
 };
 
 // 매장 리스트 API
