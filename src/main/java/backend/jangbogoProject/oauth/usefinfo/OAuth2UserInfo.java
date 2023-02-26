350e850fadb0d1db7325e807bd9587379b2af319
SBSun <46879319+SBSun@users.noreply.github.com>
2023-02-26 19:00:41 +0900

    Add OAuth2UserInfo 추상 클래스 및 각 소셜별 OAuth2UserInfo 클래스들

---
+++ src/main/java/backend/jangbogoProject/oauth/usefinfo/OAuth2UserInfo.java
@@ -0,0 +1,16 @@
+package backend.jangbogoProject.oauth.usefinfo;
+
+import java.util.Map;
+
+public abstract class OAuth2UserInfo {
+    protected Map<String, Object> attributes;
+
+    public OAuth2UserInfo(Map<String, Object> attributes){
+        this.attributes = attributes;
+    }
+
+    // 소셜 식별 값 : 구글 - "sub", 카카오 - "id", 네이버 - "id"
+    public abstract String getId();
+
+    public abstract String getNickname();
+}
