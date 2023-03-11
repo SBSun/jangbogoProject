c005ad36c4319b2cab69df791f4ec5203148279a
hyeonpearl <hc9064@gmail.com>
2023-03-11 16:22:21 +0900

    Fix: 파일명 오류 수정

--- src/main/frontend/src/pages/HomePage.js
+++ src/main/frontend/src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomeContainer from '../containers/\bHomeContainer';
+import HomeContainer from '../containers/HomeContainer';
 
 const HomePage = () => {
   return <HomeContainer />;
