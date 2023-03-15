2afcdf20c3ae555e6383a2a4d61c0a24e4791c8a
hyeonpearl <hc9064@gmail.com>
2023-03-15 21:04:38 +0900

    Fix: 코드 수정
    구글 소셜 로그인 버튼 삭제
    소셜 로그인 상태에선 계정 정보 변경 불가

--- src/main/frontend/src/containers/auth/MyPageContainer.js
+++ src/main/frontend/src/containers/auth/MyPageContainer.js
@@ -10,8 +10,9 @@ const MyPageContainer = () => {
   const navigate = useNavigate();
 
   // 엑세스, 리프레쉬 토큰 받아오기
-  const { accessToken } = useSelector(({ auth }) => ({
+  const { accessToken, loginType } = useSelector(({ auth }) => ({
     accessToken: auth.login.accessToken,
+    loginType: auth.login.loginType,
   }));
   const refreshToken = user.refreshToken;
 
@@ -22,8 +23,12 @@ const MyPageContainer = () => {
     alert('로그아웃되었습니다.');
     navigate('/', { replace: true });
   };
+  // 소셜 로그안 상태인 경우, 계정 설정 불가능
   const onAccountClick = () => {
-    navigate('/mypage/account');
+    if (loginType === 'Social')
+      return alert('소셜 로그인 상태에선 설정할 수 없습니다.');
+    console.log(loginType);
+    navigate('/mypage/account', { replace: true });
   };
   return (
     <MyPage
