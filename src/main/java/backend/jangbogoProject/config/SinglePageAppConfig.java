0de710cce65391943e1ca1d25b3e57dad5e9c262
SBSun <46879319+SBSun@users.noreply.github.com>
2023-03-21 23:36:17 +0900

    Add SinglePageAppConfig - index.html 새로고침 404 에러 해결

---
+++ src/main/java/backend/jangbogoProject/config/SinglePageAppConfig.java
@@ -0,0 +1,19 @@
+package backend.jangbogoProject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SinglePageAppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+                        .setViewName("forward:/");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.jsl\\.css)$}")
+                .setViewName("forward:/");
+    }
+}
