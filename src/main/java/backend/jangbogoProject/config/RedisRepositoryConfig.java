8dc0dd232850194296e62c95e17843d17655c849
SBSun <46879319+SBSun@users.noreply.github.com>
2023-03-28 23:59:40 +0900

    Modify RedisRepositoryConfig

--- src/main/java/backend/jangbogoProject/config/RedisRepositoryConfig.java
+++ src/main/java/backend/jangbogoProject/config/RedisRepositoryConfig.java
@@ -2,6 +2,7 @@ package backend.jangbogoProject.config;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,12 +16,17 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableRedisRepositories
 public class RedisRepositoryConfig {
-    private final RedisProperties redisProperties;
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
 
     // lettuce
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
--- src/main/java/backend/jangbogoProject/config/SinglePageAppConfig.java
+++ src/main/java/backend/jangbogoProject/config/SinglePageAppConfig.java
@@ -1,6 +1,7 @@
 package backend.jangbogoProject.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
