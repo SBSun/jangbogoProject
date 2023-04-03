295b46df01dbb9cbec6a367b999f3403c7a51fba
SBSun <46879319+SBSun@users.noreply.github.com>
2023-04-03 15:19:26 +0900

    Bug Fix Review Create API : @NotBlank 어노테이션은 String에만 사용 가능

--- src/main/java/backend/jangbogoProject/dto/ReviewRequestDTO.java
+++ src/main/java/backend/jangbogoProject/dto/ReviewRequestDTO.java
@@ -7,12 +7,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public class ReviewRequestDTO {
     @Getter
     @NoArgsConstructor
     public static class Create{
-        @NotBlank(message = "매장 아이디는 필수 입력 값입니다.")
+        @NotNull(message = "매장 아이디는 필수 입력 값입니다.")
         private Long marketId;
         @NotBlank(message = "작성자 이메일은 필수 입력 값입니다.")
         private String userEmail;
