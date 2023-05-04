5b2c694b3b70494ee86e746cb37e09de1dcccd40
SBSun <46879319+SBSun@users.noreply.github.com>
2023-05-04 10:21:36 +0900

    Modify getCommodityData Scheduled

---
+++ src/test/java/backend/jangbogoProject/controller/CategoryControllerTest.java
@@ -0,0 +1,51 @@
+package backend.jangbogoProject.controller;
+
+import backend.jangbogoProject.config.SecurityConfiguration;
+import backend.jangbogoProject.entity.category.controller.CategoryController;
+import backend.jangbogoProject.entity.category.dto.CategoryResponseDto;
+import backend.jangbogoProject.entity.category.service.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CategoryController.class)
+public class CategoryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CategoryService categoryService;
+
+    @Test
+    @WithMockUser
+    public void 모든카테고리조회() throws Exception {
+        // given
+        doReturn(
+                CategoryResponseDto.builder().build()
+        ).when(categoryService).createCategoryRoot();
+
+        mockMvc.perform(get("/categories/findAll"))
+                .andExpect(status().isOk());
+    }
+}
